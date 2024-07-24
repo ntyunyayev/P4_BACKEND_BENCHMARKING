bec8f9b6a3b89732abf4a420a533758ee43bf92e
root <nikita.tyunyayev@uclouvain.be>
2024-07-24 13:40:58 +0000

    improved script to setup dpdk

--- init_dpdk.sh
+++ init_dpdk.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./init_dpdk.sh pci_addr"
+    return
+fi
 echo 2048 | sudo tee /proc/sys/vm/nr_hugepages
 modprobe vfio-pci
 echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
-sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:04.0 --force
-sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:08.0 --force
+sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci $1 --force
