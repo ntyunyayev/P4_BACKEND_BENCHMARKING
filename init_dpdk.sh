479ff3496729afb132338ed5b50260184ca5683c
root <nikita.tyunyayev@uclouvain.be>
2024-07-24 13:36:43 +0000

    fix for network bound devices

--- init_dpdk.sh
+++ init_dpdk.sh
@@ -1,5 +1,5 @@
 echo 2048 | sudo tee /proc/sys/vm/nr_hugepages
 modprobe vfio-pci
 echo 1 > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
-sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:04.0
-sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:08.0
+sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:04.0 --force
+sudo python3 ~/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-devbind.py -b vfio-pci 0000:00:08.0 --force
