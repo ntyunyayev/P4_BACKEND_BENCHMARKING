7aa5c243de5b9a97e9c2ad2f269c21813c910a2f
Alexandre VOGEL <alexandre.vogel@uclouvain.be>
2024-07-25 11:46:45 +0200

    set mac addr

--- p4kv/client.py
+++ p4kv/client.py
@@ -10,18 +10,22 @@ class KVS(scapy.Packet):
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("src_mac", help="MAC address of the source")
+parser.add_argument("dst_mac", help="MAC address of the destination")
 parser.add_argument("dst", help="IP address of the destination")
 parser.add_argument("key", help="Key to store")
 parser.add_argument("value", help="Value to store")
 args = parser.parse_args()
 
+mac_src = args.src_mac
+mac_dst = args.dst_mac
 ip_dst = args.dst
 key = int(args.key)
 value = int(args.value)
 
 scapy.bind_layers(scapy.IP, KVS, proto=24)
 
-packet = scapy.IP(dst=ip_dst) / KVS(key=key, value=value)
+packet = scapy.Ether(src=mac_src, dst=mac_dst) / scapy.IP(dst=ip_dst) / KVS(key=key, value=value)
 print(packet.show())
 scapy.send(packet)
 
