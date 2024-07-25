a3192216c3ad90e6539ac1c1b284373350a57ca5
root <nikita.tyunyayev@uclouvain.be>
2024-07-25 13:36:13 +0000

    updated the client

--- run_client.sh
+++ run_client.sh
@@ -1,1 +1,1 @@
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ~/Workspace/Pktgen-DPDK/build_$(hostname)/app/pktgen -l 0-1  -a 0000:00:08.0 -- -m 1.0  -f pktgen_script.lua
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ~/Workspace/Pktgen-DPDK/build_$(hostname)/app/pktgen -l 0-1 -a 0000:00:04.0 -- -m [1].0 -P -f pktgen_script.lua
