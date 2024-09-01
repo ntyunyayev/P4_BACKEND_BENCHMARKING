fb6b040a13ccc64620a82c9400987daa2abda910
root <nikita.tyunyayev@uclouvain.be>
2024-09-01 16:28:55 +0000

    fix l2fwd

--- run_l2fwd.sh
+++ run_l2fwd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export RTE_INSTALL_DIR="/root/Workspace/dpdk-23.03"
-sudo RTE_INSTALL_DIR=$RTE_INSTALL_DIR ${RTE_INSTALL_DIR}/build/examples/dpdk-pipeline -l 0-1 -a 0000:00:04.0 -- -s l2fwd.cli
+sudo RTE_INSTALL_DIR=$RTE_INSTALL_DIR ${RTE_INSTALL_DIR}/build/examples/dpdk-pipeline -l 0-1 -a 0000:00:09.0 -- -s l2fwd.cli
