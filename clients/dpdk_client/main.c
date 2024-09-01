8c1bcf76a6677cb764d7ff462f8425ee1980d83d
root <nikita.tyunyayev@uclouvain.be>
2024-09-01 16:48:47 +0200

    more info on client side

--- clients/dpdk_client/main.c
+++ clients/dpdk_client/main.c
@@ -22,6 +22,7 @@ const struct rte_ether_addr dst_mac = {{0x02, 0x00, 0x83, 0x01, 0x00, 0x01}};
 
 void construct_request(struct rte_mbuf* pkt, int64_t key, int64_t val) {
     // Ether hdr processing
+    printf("======Constructing packet=========\n");
     struct rte_ether_hdr* eth_hdr = rte_pktmbuf_mtod(pkt, struct rte_ether_hdr*);
     eth_hdr->ether_type = rte_cpu_to_be_16(RTE_ETHER_TYPE_IPV4);
     rte_ether_addr_copy(&src_mac, &eth_hdr->src_addr);
@@ -29,10 +30,28 @@ void construct_request(struct rte_mbuf* pkt, int64_t key, int64_t val) {
     // IP processing
     struct rte_ipv4_hdr* ipv4_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
     ipv4_hdr->next_proto_id = kvs_protocol_id;
+    ipv4_hdr->time_to_live = 64;
+    const char ip_src[128] = "192.168.0.1";
+    const char ip_dst[128] = "192.168.0.2";
+
+    if (inet_pton(AF_INET, ip_src, &ipv4_hdr->src_addr) != 1) {
+        perror("inet_pton");
+        exit(EXIT_FAILURE);
+    }
+    if (inet_pton(AF_INET, ip_dst, &ipv4_hdr->dst_addr) != 1) {
+        perror("inet_pton");
+        exit(EXIT_FAILURE);
+    }
     // KVS protocol processing
     struct kvs_hdr* kvs_hdr = (struct kvs_hdr*)(ipv4_hdr + 1);
     kvs_hdr->key = key;
     kvs_hdr->val = val;
+    int pkt_size = 128;
+    printf("size : %d\n", pkt_size);
+    pkt->data_len = pkt_size;
+    pkt->pkt_len = pkt_size;
+    gu_print_mac_addresses(pkt);
+    printf("=================================\n");
 }
 
 static int job(void* arg) {
@@ -57,6 +76,26 @@ static int job(void* arg) {
         if (unlikely(nb_tx != 1)) {
             printf("failed to send packet\n");
             rte_pktmbuf_free(pkt);
+        } else {
+            printf("Pkt sent\n");
+        }
+    }
+}
+
+int receive_pkts(void) {
+    struct rte_mbuf* bufs[32];
+    uint16_t nb_rx;
+    while (1) {
+        nb_rx = rte_eth_rx_burst(port, 0, bufs, 32);
+        if (nb_rx == 0) {
+            continue;
+        }
+        for (int i = 0; i < nb_rx; i++) {
+            if (bufs[i]->pkt_len > 64) {
+                printf("pkt_len : %d\n", bufs[i]->pkt_len);
+                printf("data_len : %d\n", bufs[i]->data_len);
+            }
+            rte_pktmbuf_free(bufs[i]);
         }
     }
 }
@@ -83,6 +122,7 @@ int main(int argc, char* argv[]) {
 
     RTE_LCORE_FOREACH_WORKER(lcore_id) { rte_eal_remote_launch(job, (void*)ctx, lcore_id); }
 
+    receive_pkts();
     rte_eal_mp_wait_lcore();
 
     /* clean up the EAL */
