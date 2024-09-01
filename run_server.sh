11680a6fe44151750f8cbee2a9776fa4bd4ad50f
root <nikita.tyunyayev@uclouvain.be>
2024-09-01 16:50:42 +0200

    fixed run server

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ./root/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-testpmd -l 0-1  -a 0000:00:04.0  -- --portlist="0,0" --eth-peer=0,02:00:83:01:00:00
+/root/Workspace/dpdk-23.03/install_$(hostname)/bin/dpdk-testpmd -l 0-1  -a 0000:00:04.0  -- --portlist="0,0" --eth-peer=0,02:00:83:01:00:00
