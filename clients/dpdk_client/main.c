26b266b9fe211479d54535ac6a55660079501905
root <nikita.tyunyayev@uclouvain.be>
2024-09-02 20:27:54 +0200

    more work

--- clients/dpdk_client/main.c
+++ clients/dpdk_client/main.c
@@ -10,14 +10,16 @@
 
 #include "gu.h"
 #include "rte_byteorder.h"
+#include "rte_ip.h"
+#include "rte_udp.h"
 
 struct kvs_hdr {
     int64_t key;
     int64_t val;
 };
 
+#define PADDING_SIZE 16
 const uint16_t port = 0;
-const uint8_t kvs_protocol_id = 24;
 const struct rte_ether_addr src_mac = {{0x02, 0x00, 0x83, 0x01, 0x00, 0x00}};
 const struct rte_ether_addr dst_mac = {{0x02, 0x00, 0x83, 0x01, 0x00, 0x01}};
 
@@ -30,11 +32,18 @@ void construct_request(struct rte_mbuf* pkt, int64_t key, int64_t val) {
     rte_ether_addr_copy(&dst_mac, &eth_hdr->dst_addr);
     // IP processing
     struct rte_ipv4_hdr* ipv4_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
-    ipv4_hdr->hdr_checksum = rte_ipv4_cksum(ipv4_hdr);
-    ipv4_hdr->next_proto_id = kvs_protocol_id;
+    ipv4_hdr->next_proto_id = IPPROTO_UDP;
     ipv4_hdr->time_to_live = 32;
+    ipv4_hdr->total_length = rte_cpu_to_be_16(sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr) +
+                                              sizeof(struct kvs_hdr) + PADDING_SIZE);
     const char ip_src[128] = "192.168.100.1";
     const char ip_dst[128] = "192.168.100.2";
+
+    struct rte_udp_hdr* udp_hdr = (struct rte_udp_hdr*)(eth_hdr + 1);
+    udp_hdr->src_port = 10000;
+    udp_hdr->dst_port = 10001;
+    udp_hdr->dgram_cksum = 0;
+    udp_hdr->dgram_len = rte_cpu_to_be_16(sizeof(struct rte_udp_hdr) + sizeof(struct kvs_hdr) + PADDING_SIZE);
 
     if (inet_pton(AF_INET, ip_src, &ipv4_hdr->src_addr) != 1) {
         perror("inet_pton");
@@ -44,11 +53,13 @@ void construct_request(struct rte_mbuf* pkt, int64_t key, int64_t val) {
         perror("inet_pton");
         exit(EXIT_FAILURE);
     }
+    ipv4_hdr->hdr_checksum = rte_ipv4_cksum(ipv4_hdr);
     // KVS protocol processing
     struct kvs_hdr* kvs_hdr = (struct kvs_hdr*)(ipv4_hdr + 1);
     kvs_hdr->key = key;
     kvs_hdr->val = val;
-    int pkt_size = 128;
+    int pkt_size = sizeof(struct rte_ether_hdr) + sizeof(struct rte_ipv4_hdr) + sizeof(struct rte_udp_hdr) +
+                   sizeof(struct kvs_hdr) + PADDING_SIZE;
     printf("size : %d\n", pkt_size);
     pkt->data_len = pkt_size;
     pkt->pkt_len = pkt_size;
@@ -98,8 +109,12 @@ int receive_pkts(void) {
                 printf("+++++++++++++++++++++++\n");
                 struct rte_ether_hdr* eth_hdr = rte_pktmbuf_mtod(bufs[i], struct rte_ether_hdr*);
                 printf("ether_type : %d\n", rte_be_to_cpu_16(eth_hdr->ether_type));
-                struct rte_ipv4_hdr* ipv4_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
                 gu_print_mac_addresses(bufs[i]);
+                if (rte_be_to_cpu_16(eth_hdr->ether_type) == RTE_ETHER_TYPE_IPV4) {
+                    struct rte_ipv4_hdr* ipv4_hdr = (struct rte_ipv4_hdr*)(eth_hdr + 1);
+                    printf("TTL : %d\n", ipv4_hdr->time_to_live);
+
+                }
                 printf("+++++++++++++++++++++++\n");
             }
             rte_pktmbuf_free(bufs[i]);
